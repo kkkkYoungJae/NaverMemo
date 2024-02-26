24e5be55c075dd88d9de7b0ba9c7b0538beb5654
kkkkYoungJae <dudwodla123@gmail.com>
2024-02-26 18:42:36 +0900

    first commit

---
+++ voiceMemo/Model/Todo/Todo.swift
@@ -0,0 +1,18 @@
+//
+//  Todo.swift
+//  voiceMemo
+//
+
+import Foundation
+
+struct Todo: Hashable {
+    var title: String
+    var time: Date
+    var day: Date
+    var selected: Bool
+    
+    var convertedDayAndTime: String {
+        // 오늘 - 오후 03:00에 알림
+        String("\(day.formattedDay) - \(time.formattedTime)에 알림")
+    }
+}
